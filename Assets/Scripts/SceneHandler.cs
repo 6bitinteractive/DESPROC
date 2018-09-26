d6e83510304b5769d7ca2809e9e598ad4b67f619
Meitantei-NepNep <nparuli@gmail.com>
2018-09-26 19:32:48 +0800

    Add Title Screen

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public string NextScene;
+
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(NextScene);
+        SceneManager.LoadSceneAsync("PersistentData", LoadSceneMode.Additive);
+    }
+
+    public void LoadPersistentData()
+    {
+        SceneManager.LoadScene("PersistentData", LoadSceneMode.Additive);
+    }
+}
