b2cd4dfd7e69d6f4a6d45c089369810fdf509895
BENILDE\11417753 <nparuli@gmail.com>
2018-10-01 10:40:16 +0800

    Add art assets

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float speed = 3f;
+    public Transform target;
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 targetPos = target.position;
+        targetPos.z = -10;
+        transform.position = Vector3.Slerp(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
