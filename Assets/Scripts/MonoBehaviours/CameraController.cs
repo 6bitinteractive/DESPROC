0ec70aefe462567e6d5747164fc9985630b181f2
Lim <michelle.lim@benilde.edu.ph>
2018-10-03 21:22:44 +0800

    Add alternative camera controller (not set)

---
+++ Assets/Scripts/MonoBehaviours/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 15f;
+    public float minDistance;
+    public GameObject target;
+    public Vector3 offset;
+
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position + offset;
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+            float interpVelocity = targetDirection.magnitude * speed;
+            targetPos = (transform.position) + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.25f);
+        }
+    }
+}
+
+// Source: https://gist.github.com/jeremyroelfs/6201759348964ec60b135cf60c01ddf2
