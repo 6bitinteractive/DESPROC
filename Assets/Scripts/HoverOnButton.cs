6dfc2d89b86fd77722f5053fc293c5fcdf23c10c
Meitantei-NepNep <nparuli@gmail.com>
2018-11-01 13:35:21 +0800

    Add new shop sprites

---
+++ Assets/Scripts/HoverOnButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // Required when using Event data. Use this to detect mouseover, etc. on UI elements.
+using UnityEngine.UI;
+
+public class HoverOnButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler // Required for OnPointer methods
+{
+    public Image HoverImage;
+    public Image MouseDownImage;
+
+    void Awake()
+    {
+        HoverImage.enabled = false;
+        MouseDownImage.enabled = false;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        HoverImage.enabled = true;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        HoverImage.enabled = false;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        MouseDownImage.enabled = true;
+        HoverImage.enabled = false;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        //MouseDownImage.enabled = false;
+    }
+}
