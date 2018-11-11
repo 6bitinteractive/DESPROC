54f32197dd6f0262f3a3d7b1a6b1bcc7c198999c
Lim <michelle.lim@benilde.edu.ph>
2018-11-11 18:03:48 +0800

    Clean up Scenes, Scripts folder
    - Remove PersistentData scene
    - Archive GlobalData script
    - Remove code related to Persistent data in SceneHandler script
    - Remove junk files

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
-    public bool LoadPersistentData;
-
-    void Start()
-    {
-        if (LoadPersistentData)
-        {
-            SceneManager.LoadScene("PersistentData", LoadSceneMode.Additive);
-        }
-    }
-
     public void ExitScene()
     {
         Application.Quit();
