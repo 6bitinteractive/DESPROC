b5a8fd453feee9a1bac762deb8b6dd26f62f6b5e
Meitantei-NepNep <nparuli@gmail.com>
2018-11-18 14:45:08 +0800

    Add shuffle script

---
+++ Assets/Scripts/Shuffle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffle : MonoBehaviour
+{
+   public static void ShuffleArray<T>(T[] array)
+    {
+        int p = array.Length;
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int r = Random.Range(0, i);
+            T tmp = array[i];
+            array[i] = array[r];
+            array[r] = tmp;
+        }
+    }
+}
