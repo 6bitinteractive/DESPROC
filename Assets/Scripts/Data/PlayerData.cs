e22e54060d1851444dc28661e310b3d119a60d7c
Lim <michelle.lim@benilde.edu.ph>
2018-11-21 22:06:40 +0800

    Add resetting of scriptable object data

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -6,14 +6,45 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/PlayerData", fileName = "PlayerData")]
 public class PlayerData : Data
 {
-    public float Tortgold;
-    public float Tortpoints;
-    public int InventoryCapacity;
-    public float MovementSpeed;
-    public float PickupSpeed;
-    public float Luck;
-    public Plastic[] Inventory;
-    public Plastic[] Bin;
-    public string PreviousScene;
-    public Vector2 PreviousPosition;
+    [SerializeField] protected float BaseTortgold;
+    [SerializeField] protected float BaseTortpoints;
+    [SerializeField] protected int BaseInventoryCapacity;
+    [SerializeField] protected float BaseMovementSpeed;
+    [SerializeField] protected float BasePickupSpeed;
+    [SerializeField] protected float BaseLuck;
+
+    [HideInInspector] public float Tortgold;
+    [HideInInspector] public float Tortpoints;
+    [HideInInspector] public int InventoryCapacity;
+    [HideInInspector] public float MovementSpeed;
+    [HideInInspector] public float PickupSpeed;
+    [HideInInspector] public float Luck;
+    [HideInInspector] public List<Plastic> Inventory;
+    [HideInInspector] public List<Plastic> Bin;
+    [HideInInspector] public string PreviousScene;
+    [HideInInspector] public Vector2 PreviousPosition;
+
+    protected void OnEnable()
+    {
+        Debug.Log("Player Data scriptable object has been reset.");
+
+        Tortgold = BaseTortgold;
+        Tortpoints = BaseTortpoints;
+        InventoryCapacity = BaseInventoryCapacity;
+        MovementSpeed = BaseMovementSpeed;
+        PickupSpeed = BasePickupSpeed;
+        Luck = BaseLuck;
+        PreviousScene = string.Empty;
+        PreviousPosition = Vector2.zero;
+
+        if (Inventory != null)
+            Inventory.Clear();
+        else
+            Inventory = new List<Plastic>();
+
+        if (Bin != null)
+            Bin.Clear();
+        else
+            Bin = new List<Plastic>();
+    }
 }
