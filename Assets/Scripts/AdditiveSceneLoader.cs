8489c1368e32bdbb4b881d399282995d9b0851cd
Lim <michelle.lim@benilde.edu.ph>
2018-11-23 12:19:54 +0800

    Change to additive loading when entering the shop at Island_01

---
+++ Assets/Scripts/AdditiveSceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdditiveSceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(SwitchScene(sceneName));
+    }
+
+    private IEnumerator SwitchScene(string sceneName)
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        yield return load; // wait until new scene has been loaded
+
+        Scene scene = SceneManager.GetSceneByName(sceneName);
+        SceneManager.SetActiveScene(scene); // set the newly loaded scene as the active scene; see reference below
+        Debug.Log("Current active scene: " + SceneManager.GetActiveScene().name);
+    }
+}
+
+// Reference
+// First scene is still the active scene when loading additively
+// https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.SetActiveScene.html
--- Assets/Scripts/EnterArea.cs
+++ Assets/Scripts/EnterArea.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class EnterArea : MonoBehaviour
 {
-    public string NextScene;
+    [SerializeField] private GameEvent OnEnterArea;
+    [SerializeField] private string nextScene;
+    [SerializeField] private LayerMask playerLayerMask;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        int collisionLayerMask = 1 << collision.gameObject.layer;
+
         // If collides with player display text
-        if (collision.gameObject.layer == 8)
+        if (collisionLayerMask == playerLayerMask.value)
         {
-            SceneManager.LoadScene(NextScene);
+            //SceneManager.LoadScene(nextScene);
+            OnEnterArea.Raise();
         }
     }
 }
+
+// Reference: Check layer mask for collision
+// http://answers.unity.com/answers/454913/view.html
+// https://gamedev.stackexchange.com/questions/119667/how-to-get-the-gameobjects-layermask
