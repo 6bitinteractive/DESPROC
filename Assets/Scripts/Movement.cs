8afdf47cf9179dccc89066d5745307dc345376e0
Meitantei-NepNep <nparuli@gmail.com>
2018-11-24 21:46:24 +0800

    Add blend tree for player animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,6 +9,8 @@ public class Movement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Animator animator;
+    private float LastXDirection;
+    private float LastYDirection;
 
     void Start()
     {
@@ -21,11 +23,43 @@ public class Movement : MonoBehaviour
         // Checks if player then player animations (Note this is a placeholder since other moving entites dont have animations yet)
         if (gameObject.layer == 8)
         {
-            animator.SetFloat("xSpeed", Mathf.Abs(rb.velocity.x));
-            animator.SetFloat("ySpeed", Mathf.Abs(rb.velocity.y));
+           // float heading = Vector3.Normalize(xDirection + yDirection);
+            //animator.SetFloat("xDirection", Mathf.Abs(rb.velocity.x));
+          //  animator.SetFloat("yDirection", Mathf.Abs(rb.velocity.y));
+
+             UpdateAnimation(xDirection, yDirection); 
         }
 
         // Moves the entity
         rb.velocity = new Vector3(xDirection * xSpeed, yDirection * ySpeed);
+    }
+
+    private void UpdateAnimation(float xDirection, float yDirection)
+    {
+        // Check if no longer moving
+        if (xDirection == 0 && yDirection == 0)
+        {
+            // Set last directions
+            animator.SetFloat("LastXDirection", LastXDirection);
+            animator.SetFloat("LastYDirection", LastYDirection);
+
+            // Become idle
+            animator.SetBool("isMoving", false);
+        }
+
+        // Else if moving
+        else
+        {
+            // Set Last directions
+            LastXDirection = xDirection;
+            LastYDirection = yDirection;
+
+            // Move sprite
+            animator.SetBool("isMoving", true);
+        }
+
+        animator.SetFloat("xDirection", xDirection);
+        animator.SetFloat("yDirection", yDirection);
+
     }
 }
