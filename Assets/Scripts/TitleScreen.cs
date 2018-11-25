0a77faa5c3cc821c0713edee4f7a5385cbe3587e
Meitantei-NepNep <nparuli@gmail.com>
2018-11-25 22:56:52 +0800

    Add TitleScreen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public string NextScene;
+    void Update()
+    {
+        if (Input.anyKeyDown) Invoke("LoadScene", 0.5f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+}
