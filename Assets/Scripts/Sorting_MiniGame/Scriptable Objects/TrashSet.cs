1ad1aa9324952a7e3ee508d1ce48db6908b80278
Azunyan Taichou <meitanteinep@gmail.com>
2019-01-28 11:34:32 +0800

    Add sorting minigame

---
+++ Assets/Scripts/Sorting_MiniGame/Scriptable Objects/TrashSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TrashSet", fileName = "TrashSet00")]
+
+public class TrashSet : ScriptableObject
+{
+    public Sprite[] steps;
+}
