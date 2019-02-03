a55a87243135775f244a9071d48e73a435d58c1b
HP\Angelica <mariaangelica.canta@benilde.edu.ph>
2019-02-03 15:57:57 +0800

    Added tutorial scene, dialogue manager, dialogue box animatiions and SFX

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+    private Queue<string> sentences;
+
+	void Start ()
+    {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+
+        // Clear previous sentences
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        // If no sentences remain, end dialogue
+        if(sentences.Count <= 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        // Makes sure all sentence animations are stopped before typing in new sentence
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    // Types sentence per letter
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            // Adds letter to dialogue text string every after 1 frame
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
