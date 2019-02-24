ee2b78db5f54514bed25ff149dbbc104ee622806
Azunyan Taichou <meitanteinep@gmail.com>
2019-02-24 14:23:04 +0800

    Add turtle choke

--- Assets/Scripts/Controller/TurtleController.cs
+++ Assets/Scripts/Controller/TurtleController.cs
@@ -10,12 +10,17 @@ public class TurtleController : MonoBehaviour
     Wander wander;
 
     public float Speed;
+    public float ChokeDuration = 5;
 
-    private Transform plastic;
+    private bool isChoking;
+    private Collider2D plastic;
+    private Rigidbody2D plasticRB;
 
     [SerializeField] private LayerMask plasticLayerMask;
     [SerializeField] private TurtleChildCollision body;
     [SerializeField] private TurtleChildCollision sight;
+    [SerializeField] private GameEvent OnDeath;
+    [SerializeField] private GameEvent OnChoke;
 
     public enum FSMState
     {
@@ -44,16 +49,30 @@ public class TurtleController : MonoBehaviour
             case FSMState.Choke: ChokeState(); break;
             case FSMState.Dead: DeadState(); break;
         }
+
+        //Debugging Purposes
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isChoking = false;
+        }
     }
 
     private void DeadState()
     {
-        throw new NotImplementedException();
+       Debug.Log("Dead");
+     //  OnDeath.Raise();
     }
 
     private void ChokeState()
     {
-        throw new NotImplementedException();
+      
+        if (isChoking)
+        {
+            StartCoroutine(Choke());
+          //  OnChoke.Raise();
+        }
+
+        else curState = FSMState.Wander;
     }
 
     private void WanderState()
@@ -63,11 +82,10 @@ public class TurtleController : MonoBehaviour
 
     private void ChaseState()
     {
-        Debug.Log("Chasing");
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(plastic.position.x, transform.position.y), Speed * Time.deltaTime); // Move Towards target
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(plastic.transform.position.x, plastic.transform.position.y), Speed * Time.deltaTime); // Move Towards target
         
         // Rotate Towards new Target
-        Vector2 direction = plastic.position - transform.position;
+        Vector2 direction = plastic.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
@@ -75,25 +93,41 @@ public class TurtleController : MonoBehaviour
 
     public void OnChildCollision(TurtleChildCollision childPart, Collider2D collider)
     {
-        plastic = collider.transform;
+        plastic = collider;
+        plasticRB = plastic.GetComponent<Rigidbody2D>();
 
-        // Checks collisions with Body
         int collisionLayerMask = 1 << collider.gameObject.layer;
 
-        // If collides with Plastic
-        if (collisionLayerMask == plasticLayerMask.value)
+        // Check if already choking
+        if (!isChoking)
         {
-            // If collides with Body
-            if (childPart == body)
+            // If collides with Plastic
+            if (collisionLayerMask == plasticLayerMask.value)
             {
-              //  curState = FSMState.Choke;
-            }
+                // If collides with Body
+                if (childPart == body)
+                {
+                    isChoking = true;
+                    curState = FSMState.Choke;
+                    plasticRB.constraints = RigidbodyConstraints2D.FreezePosition;
+                }
 
-            // If collides with Sight
-            if (childPart == sight)
-            {
-               curState = FSMState.Chase;
+                // If collides with Sight
+                if (childPart == sight)
+                {
+                    curState = FSMState.Chase;
+                }
             }
+        }      
+    }
+
+    IEnumerator Choke()
+    {
+        while (isChoking)
+        {
+            yield return new WaitForSeconds(ChokeDuration);
+            plastic.gameObject.SetActive(false);
+            curState = FSMState.Dead;
         }
     }
 }
