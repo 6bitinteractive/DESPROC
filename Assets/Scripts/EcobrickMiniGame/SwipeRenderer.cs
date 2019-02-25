a0b69edbdc639ae2c432423bdbe34e76b84793d1
Lim <michelle.lim@benilde.edu.ph>
2019-02-25 09:21:01 +0800

    Add automatic clearing of rendered swipe

--- Assets/Scripts/EcobrickMiniGame/SwipeRenderer.cs
+++ Assets/Scripts/EcobrickMiniGame/SwipeRenderer.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class SwipeRenderer : MonoBehaviour
 {
+    [SerializeField] private float clearDelayTime = 1f;
     [SerializeField] private float zOffset = 10f;
 
     private LineRenderer lineRenderer;
@@ -30,6 +31,18 @@ public class SwipeRenderer : MonoBehaviour
         Vector3[] positions = new Vector3[2];
         positions[0] = Camera.main.ScreenToWorldPoint(new Vector3(data.StartPosition.x, data.StartPosition.y, zOffset));
         positions[1] = Camera.main.ScreenToWorldPoint(new Vector3(data.EndPosition.x, data.EndPosition.y, zOffset));
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPositions(positions);
+
+        Invoke("ClearLineRender", clearDelayTime);
+    }
+
+    private void ClearLineRender()
+    {
+        Vector3[] positions = new Vector3[2];
+        positions[0] = Vector3.zero;
+        positions[1] = Vector3.zero;
 
         lineRenderer.positionCount = 2;
         lineRenderer.SetPositions(positions);
