727e39936b5a7afe129d0b16f42c194191bac9ce
Lim <michelle.lim@benilde.edu.ph>
2019-03-10 11:03:42 +0800

    Add persistent audio system

---
+++ Assets/Scripts/Audio/PersistentAudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentAudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource bgm;
+    [SerializeField] private AudioSource ambient;
+
+    public AudioSource AudioSource(AudioType audioType)
+    {
+        switch (audioType)
+        {
+            case AudioType.BGM:
+                return bgm;
+            case AudioType.Ambient:
+                return ambient;
+        }
+
+        return null;
+    }
+
+    public void SwitchAudio(AudioClip clip, AudioType type)
+    {
+        AudioSource audioSource = AudioSource(type);
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
