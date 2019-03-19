9304e1807e424544ce3dab62e2890bf600edd970
HP\Angelica <mariaangelica.canta@benilde.edu.ph>
2019-03-19 16:14:22 +0800

    Add tutorial slideshow (Test in requirements scene)

---
+++ Assets/Scripts/Dialogue/ImageSlideshow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSlideshow : MonoBehaviour
+{
+    [Header("Display")]
+    public Sprite[] spriteArray;
+    public Image displayImage;
+    public Button nextImage;
+    public Button previousImage;
+
+    [Header("Dialogue")]
+    public DialogueTrigger dialogue;
+    public Text dialogueText;
+
+    private int i = 0;
+
+    void Update()
+    {
+        displayImage.sprite = spriteArray[i % spriteArray.Length];
+        dialogueText.text = dialogue.dialogueArray[0].sentenceArray[i % dialogue.dialogueArray[0].sentenceArray.Length].sentence.ToString();
+    }
+
+    public void BtnNext()
+    {
+        i++;
+    }
+
+    public void BtnPrev()
+    {
+         i--;
+    }
+}
