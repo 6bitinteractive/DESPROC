c3ac58104dc134dc99fbf534eb7c264d74d3443a
Lim <michelle.lim@benilde.edu.ph>
2019-03-23 10:17:28 +0800

    Add handler for interact text (change depending on platform);
    Change sort order for Persistent fade canvas

---
+++ Assets/Scripts/UI/TextHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHelper : MonoBehaviour
+{
+    [SerializeField] private string pcText;
+    [SerializeField] private string mobileText;
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+
+#if UNITY_STANDALONE
+        text.text = pcText;
+#endif
+
+#if UNITY_ANDROID || UNITY_IOS
+        text.text = mobileText;
+#endif
+    }
+}
