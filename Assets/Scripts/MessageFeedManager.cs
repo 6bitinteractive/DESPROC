48c1a042b4ea76910a2b6427079feaa56c9a36a5
Lim <michelle.lim@benilde.edu.ph>
2019-04-03 22:15:25 +0800

    Add image background to message feed to improve readability

--- Assets/Scripts/MessageFeedManager.cs
+++ Assets/Scripts/MessageFeedManager.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class MessageFeedManager : MonoBehaviour
 {
-
     [SerializeField] private GameObject messagePrefab;
+    private Image background;
 
     private static MessageFeedManager instance;
 
@@ -22,11 +22,23 @@ public class MessageFeedManager : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        background = GetComponent<Image>();
+    }
+
     public void WriteMessage(string message)
     {
+        background.enabled = true; // Show background
         GameObject gameObject = Instantiate(messagePrefab, transform);
         gameObject.GetComponent<Text>().text = message;
         gameObject.transform.SetAsFirstSibling();// Moves text upwards
-        Destroy(gameObject, 2); // Destroy after 2 seconds
+        Destroy(gameObject, 2.3f); // Destroy after a few seconds
+        Invoke("HideBackground", 2.3f); // Hide background
+    }
+
+    private void HideBackground()
+    {
+        background.enabled = false;
     }
 }
