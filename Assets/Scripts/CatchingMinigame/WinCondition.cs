344a199399a7f0c011ff4e01eb6763bd00628274
Lim <michelle.lim@benilde.edu.ph>
2019-04-03 23:03:48 +0800

    Fix: player can still progress without collecting any plastic in Catching minigame

---
+++ Assets/Scripts/CatchingMinigame/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// When timer reaches 0 and there are still turtles alive, check if player did collect some plastic
+public class WinCondition : MonoBehaviour
+{
+    [SerializeField] private TurtleTale.SessionData sessionData;
+    [SerializeField] private int requiredPlastic = 3;
+
+    public UnityEvent OnVictory = new UnityEvent();
+    public UnityEvent OnGameOver = new UnityEvent();
+
+    public void VerifyWin()
+    {
+        if (sessionData.CollectedPlastic.Count >= requiredPlastic)
+            OnVictory.Invoke();
+        else
+            OnGameOver.Invoke();
+    }
+}
