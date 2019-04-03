a28ba740c62b25499f67bb21ae95ef7e33b0b725
Lim <michelle.lim@benilde.edu.ph>
2019-04-03 20:55:17 +0800

    Add turtle count panel for Catching minigame

---
+++ Assets/Scripts/CatchingMinigame/TurtleCount.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class TurtleCount : MonoBehaviour
+{
+    [SerializeField] private int count = 5;
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        text.text = count.ToString();
+    }
+
+    public void Decrease()
+    {
+        count--;
+        text.text = count.ToString();
+    }
+}
