e444d03e0c94a3c7f7feb344a900c5b3afb77db6
Azunyan Taichou <meitanteinep@gmail.com>
2019-09-11 16:02:34 +0800

    Add tap to talk to npc functionallity in "TapMovementTest" scene

--- Assets/Scripts/NPCPrompt.cs
+++ Assets/Scripts/NPCPrompt.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCPrompt : MonoBehaviour
 {
     public GameEvent OnNPCCollision;
     public GameEvent OnNPCCollisionExit;
+    // The commented lines will be uncommented once we start to implement it to every scene
+   // [SerializeField] Image TalkSymbol;
+  //  [SerializeField] Canvas canvas;
+
+    private void Awake()
+    {
+      //  canvas.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // If colliding with player
         if (collision.gameObject.layer == 8)
         {
+           // canvas.enabled = true;
             OnNPCCollision.Raise();
         }
     }
@@ -21,6 +31,7 @@ public class NPCPrompt : MonoBehaviour
         // If colliding with player
         if (collision.gameObject.layer == 8)
         {
+           // canvas.enabled = false;
             OnNPCCollisionExit.Raise();
         }
     }
