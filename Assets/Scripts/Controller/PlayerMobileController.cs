5ac528b26911dedc9db1b20b1e6e9f9b3c3d02a0
Azunyan Taichou <meitanteinep@gmail.com>
2019-09-26 14:41:55 +0800

    Prevent player from moving when clicking on quest log

--- Assets/Scripts/Controller/PlayerMobileController.cs
+++ Assets/Scripts/Controller/PlayerMobileController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 // This script is a placeholder for playermobilecontroller simply to test tap to move
 public class PlayerMobileController : MonoBehaviour
@@ -27,6 +28,10 @@ public class PlayerMobileController : MonoBehaviour
     {
         if (movement.enabled == true && Input.GetMouseButtonDown(0))
         {
+            // Check if hovering over UI elements
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
             CastRay();
         }
     }
@@ -50,14 +55,24 @@ public class PlayerMobileController : MonoBehaviour
         Vector3 worldPoint = MainCamera.ScreenToWorldPoint(Input.mousePosition);
         worldPoint.z = MainCamera.transform.position.z;
         Ray ray = new Ray(worldPoint, new Vector3(0, 0, 1));
-        RaycastHit2D hit = Physics2D.GetRayIntersection(ray, LayerMask);
-
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+       
         if (hit)
         {
             targetPos = hit.point; // Set target pos
             direction.CheckDirection(targetPos.x); // Face target pos
             isMoving = true; // Enable Move function
         }
+
+        /*  int hitLayerMask = hit.collider.gameObject.layer;
+        if ((LayerMask.value & 1 << hitLayerMask) != 0)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            targetPos = hit.point; // Set target pos
+            direction.CheckDirection(targetPos.x); // Face target pos
+            isMoving = true; // Enable Move function
+        }
+        */
     }
 
     private void OnCollisionStay2D(Collision2D collision)
