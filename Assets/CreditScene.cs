f18ff1c64fd8fdf0c8632a0987dc27c16375eeb5
Padorable <cesarpaolo.donato@benilde.edu.ph>
2020-04-29 14:24:02 +0800

    Added Credits Moved Scripts to the right folder Added additional quest after fixing bridge Lowered Volume of right and wrong in ecobrick minigame

---
+++ Assets/CreditScene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScene : MonoBehaviour {
+
+    public CanvasGroup CreditsGroup;
+    public float LengthOfFadeOut;
+    public UnityEngine.Events.UnityEvent OnEndOfCredits;
+    private bool isFadingOut = false;
+
+    private void Start()
+    {
+        StartCoroutine(CheckIfDone());
+    }
+
+    public void StartFadeOut()
+    {
+        if (isFadingOut) return;
+        isFadingOut = true;
+
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator CheckIfDone()
+    {
+        Animator anim = GetComponent<Animator>();
+        while (true)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("CreditsAnimation") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >=.99f)
+            {
+                StartFadeOut();
+                break;
+            }
+            yield return null;
+        }
+    }
+	IEnumerator FadeOut()
+    {
+        float elapsedTime = 0;
+        while (CreditsGroup.alpha != 0)
+        {
+            elapsedTime += Time.deltaTime;
+
+            CreditsGroup.alpha = Mathf.Clamp(1 - (elapsedTime / LengthOfFadeOut),0,1);
+
+            yield return null;
+        }
+        if(OnEndOfCredits != null)
+            OnEndOfCredits.Invoke();
+    }
+}
---
+++ Assets/Scripts/DelayEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayEvent : MonoBehaviour {
+
+    public float SecondsBeforeRunning = 0;
+
+    public UnityEngine.Events.UnityEvent EventsToRun;
+
+    public void Run()
+    {
+        StartCoroutine(delayRun());
+    }
+    IEnumerator delayRun()
+    {
+        yield return new WaitForSeconds(SecondsBeforeRunning);
+        EventsToRun.Invoke();
+    }
+}
